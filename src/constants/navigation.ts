c6808c055e737a959a7b38bfc20a7dca5d8d42fd
samuel.hasiholan <samuel.hasiholan@teknologi.belajar.id>
2026-06-17 22:56:40 +0700

    chore: initialize HKBP website

---
+++ src/constants/navigation.ts
@@ -0,0 +1,30 @@
+export const NAVIGATION = [
+  {
+    title: "Beranda",
+    href: "/",
+  },
+  {
+    title: "Tentang",
+    href: "/tentang",
+  },
+  {
+    title: "Organisasi",
+    href: "/organisasi",
+  },
+  {
+    title: "Jadwal Pelayanan",
+    href: "/jadwal-pelayanan",
+  },
+  {
+    title: "Warta Jemaat",
+    href: "/warta-jemaat",
+  },
+  {
+    title: "Berita",
+    href: "/berita",
+  },
+  {
+    title: "Kontak",
+    href: "/kontak",
+  },
+];
